ef688b10ff9315a054bcf2176118f3e272f4bbeb
Ignacio Quercetti <i.quercetti@alumno.edu.um.ar>
2025-08-25 18:01:39 -0300

    Sistema de turnos

--- backgammon-computacion-2025/src/game/board.py
+++ backgammon-computacion-2025/src/game/board.py
@@ -1,12 +1,11 @@
 from game.player import player
 from game.dice import dice
 
-class Board:
-    def __init__(self):
-        self.players = []
-        self.current_turn = 0
-        self.board_state = self.initialize_board()
-
-    def initialize_board(self):
-        # Initialize the board state
-        return [[None for _ in range(8)] for _ in range(8)]
+class board:
+    def __init__(self, player1: player, player2: player):
+        self.player1 = player1
+        self.player2 = player2
+        self.dice1 = dice()
+        self.dice2 = dice()
+        self.winner = None
+        self.current_player = player1
--- backgammon-computacion-2025/src/game/player.py
+++ backgammon-computacion-2025/src/game/player.py
@@ -1,11 +1,7 @@
 class player:
     def __init__(self, name):
         self.name = name
-        self.score = 0
-        self.is_active = True
-
-    def deactivate(self):
-        self.is_active = False
-        if not self.is_active:
-            self.score =  0
-
+        self.winstreak = 0
+        self.gamesplayed = 0
+        self.wins = 0
+        self.losses = 0
